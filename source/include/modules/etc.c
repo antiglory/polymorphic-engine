e55cc8e4c6075970f4814e8ff8d2128329f92d6c
dezska <104264903+antiglory@users.noreply.github.com>
2024-12-19 17:18:58 -0300

    update etc.c

--- source/include/modules/etc.c
+++ source/include/modules/etc.c
@@ -5,7 +5,8 @@ void cprintf(const char* cp_stp, ...)
     va_list cp_args;
     va_start(cp_args, cp_stp);
 
-    for (int c = 0; cp_stp[c] != '\0'; c++) {
+    for (int c = 0; cp_stp[c] != '\0'; c++)
+    {
         if (cp_stp[c] == '*')
             if (cp_stp[c-1] == '[' && cp_stp[c+1] == ']')
                 printf("\033[1;33m*\033[0m");
@@ -31,20 +32,26 @@ void cprintf(const char* cp_stp, ...)
                 printf("\033[1;35m=\033[0m");
             else
                 putchar('=');
-        else if (cp_stp[c] == '%') {
-            if (cp_stp[c+1] == 'd') {
+        else if (cp_stp[c] == '%')
+        {
+            if (cp_stp[c+1] == 'd')
+            {
                 int32_t cp_val = va_arg(cp_args, int32_t);
 
                 printf("%d", cp_val);
 
                 c += 1;
-            } else if (cp_stp[c+1] == 's') {
+            }
+            else if (cp_stp[c+1] == 's')
+            {
                 char cp_val = va_arg(cp_args, char);
 
                 printf("%s", cp_val);
 
                 c += 1;
-            } else if (cp_stp[c+1] == 'z') {
+            }
+            else if (cp_stp[c+1] == 'z')
+            {
                 if (cp_stp[c+2] == 'u') {
                     size_t cp_val = va_arg(cp_args, size_t);
 
@@ -59,14 +66,19 @@ void cprintf(const char* cp_stp, ...)
                     c += 2;
                 } else
                     putchar('%');
-            } else if (cp_stp[c+1] == 'p') {
+            }
+            else if (cp_stp[c+1] == 'p')
+            {
                 void* cp_val = va_arg(cp_args, void*);
 
                 printf("\033[0;32m%p\033[0m", cp_val);
 
                 c += 1;
-            } else if (cp_stp[c+1] == 'l') {
-                if (cp_stp[c+2] == 'x') {
+            }
+            else if (cp_stp[c+1] == 'l')
+            {
+                if (cp_stp[c+2] == 'x')
+                {
                     unsigned long cp_val = va_arg(cp_args, unsigned long);
 
                     printf("\033[0;32m%lx\033[0m", cp_val);
